7788d44535cfddeb807c16e1179deb7b051f0319
Hitesh Choudhary <hiteshchoudhary2508@gmail.com>
2025-02-19 22:08:36 +0530

    frontend-deploy

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,7 @@ connection(url).then(() => {
     console.log("there is some issue in connecting database")
 })
 app.use(cors({
-    origin: 'schedule-manager-f12sanbyz-hitesh25s-projects.vercel.app', // Frontend URL
+    origin: 'https://schedule-manager-kdb2eoqrj-hitesh25s-projects.vercel.app/', // Frontend URL
     credentials: true, // Allow cookies to be sent
 }));
 // app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
